e7badf0fd950f24fa856d1e086fc4d4a6a38ea90
balaji r v <balurv1997@gmail.com>
2024-01-09 13:40:30 +0530

    impl factorial calculation using multhithreads

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ import java.util.Random;
 
 public class Main {
     public static final int MAX_PASSWORD = 9999;
+
     public static void main(String[] args) throws InterruptedException {
         Random random = new Random();
         Vault vault = new Vault(random.nextInt(MAX_PASSWORD));
@@ -18,18 +19,21 @@ public class Main {
         list.add(descendingHackerThread);
         list.add(randomHackerThread);
 //        list.add(policeThread);
-        for(Thread thread : list){
+        for (Thread thread : list) {
             thread.start();
         }
         policeThread.start();
 
     }
-    private static class Vault{
+
+    private static class Vault {
         private int password;
-        public Vault(int password){
+
+        public Vault(int password) {
             this.password = password;
         }
-        public boolean isCorrectPassword(int guess){
+
+        public boolean isCorrectPassword(int guess) {
             try {
                 Thread.sleep(5);
             } catch (InterruptedException e) {
@@ -38,72 +42,81 @@ public class Main {
             return this.password == guess;
         }
     }
-    private static class HackerThread extends Thread{
+
+    private static class HackerThread extends Thread {
         protected Vault vault;
-        public HackerThread(Vault vault){
+
+        public HackerThread(Vault vault) {
             this.vault = vault;
             this.setName(this.getClass().getSimpleName());
             this.setPriority(Thread.MAX_PRIORITY);
         }
 
         @Override
-        public void start(){
-            System.out.println("current Thread:"+ this.getName());
+        public void start() {
+            System.out.println("current Thread:" + this.getName());
             super.start();
         }
     }
-    private static class AscendingHackerThread extends  HackerThread{
+
+    private static class AscendingHackerThread extends HackerThread {
 
         public AscendingHackerThread(Vault vault) {
             super(vault);
         }
+
         @Override
-        public void run(){
-            for(int guess = 0; guess < MAX_PASSWORD; guess++){
-                if(vault.isCorrectPassword(guess)){
-                    System.out.println(this.getName()+" guessed the password"+guess);
+        public void run() {
+            for (int guess = 0; guess < MAX_PASSWORD; guess++) {
+                if (vault.isCorrectPassword(guess)) {
+                    System.out.println(this.getName() + " guessed the password" + guess);
                     System.exit(0);
                 }
             }
         }
     }
-    private static class DescendingHackerThread extends HackerThread{
+
+    private static class DescendingHackerThread extends HackerThread {
 
         public DescendingHackerThread(Vault vault) {
             super(vault);
         }
+
         @Override
-        public void run(){
-            for(int guess = MAX_PASSWORD ; guess>= 0; guess--){
-                if(vault.isCorrectPassword(guess)){
-                    System.out.println(this.getName()+" guessed the password"+guess);
+        public void run() {
+            for (int guess = MAX_PASSWORD; guess >= 0; guess--) {
+                if (vault.isCorrectPassword(guess)) {
+                    System.out.println(this.getName() + " guessed the password" + guess);
                     System.exit(0);
                 }
             }
         }
     }
-    private static class RandomHackerThread extends HackerThread{
+
+    private static class RandomHackerThread extends HackerThread {
         public RandomHackerThread(Vault vault) {
             super(vault);
         }
+
         @Override
-        public void run(){
-            while (true){
+        public void run() {
+            while (true) {
                 Random random = new Random();
                 // Generate a random number within the 0 .. max_password range
                 int randomNumber = random.nextInt(MAX_PASSWORD - 0 + 1) + 0;
 
-                if(vault.isCorrectPassword(randomNumber)){
-                    System.out.println(this.getName()+" guessed the password"+ randomNumber);
+                if (vault.isCorrectPassword(randomNumber)) {
+                    System.out.println(this.getName() + " guessed the password" + randomNumber);
                     System.exit(0);
                 }
             }
         }
     }
-    private static class PoliceThread extends Thread{
+
+    private static class PoliceThread extends Thread {
         @Override
-        public void run(){
-            for(int i = 10; i > 0; i--){
+        public void run() {
+            for (int i = 10; i > 0; i--) {
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
---
+++ src/main/java/org/example/practice/factorialExample/Main.java
@@ -0,0 +1,63 @@
+package org.example.practice.factorialExample;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        List<Long> list = Arrays.asList(919909L,3450L, 2539L, 8782L, 2389L, 9970L, 2301L, 5899L );
+        List<FactorailThread> threads = new ArrayList<>();
+        for (long input : list){
+            threads.add(new FactorailThread(input));
+        }
+        for(FactorailThread thread : threads){
+            thread.start();
+        }
+        for(FactorailThread thread : threads){
+            thread.join(2000);
+        }
+
+        for(int i = 0; i < list.size(); i++){
+            FactorailThread factorailThread = threads.get(i);
+            if(factorailThread.isFinished()){
+                System.out.println("factorial of "+list.get(i)+" is "+factorailThread.getResult());
+            }
+            else {
+                System.out.println("The calculation for "+list.get(i)+" is still in process");
+            }
+        }
+
+        System.exit(0);
+    }
+}
+
+class FactorailThread extends Thread{
+    private Long inputNumber;
+    private BigInteger result = BigInteger.ZERO;
+    private boolean isFinished = false;
+    public FactorailThread(Long inputNumber){
+        this.inputNumber = inputNumber;
+    }
+    @Override
+    public void run(){
+        this.result = calculateFactorial(inputNumber);
+        this.isFinished = true;
+    }
+
+    private BigInteger calculateFactorial(long inputNumber){
+        BigInteger res = BigInteger.ONE;
+        for(long i = inputNumber; i> 0; i--){
+            res = res.multiply(new BigInteger(Long.toString(i)));
+        }
+        return res;
+    }
+
+    public boolean isFinished(){
+        return isFinished;
+    }
+    public BigInteger getResult(){
+        return result;
+    }
+}
